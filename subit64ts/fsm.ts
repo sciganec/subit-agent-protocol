78ca688160a87a07e14f1d4e5e0b908f466e1c70
sciganec <s.ciganec@gmail.com>
2026-02-08 14:38:35 -0500

    Delete subit64ts/fsm.ts

--- subit64ts/fsm.ts
+++
@@ -1,8 +0,0 @@
-export class FSM {
-  constructor(private transitions: Record<number, Record<string, number>>) {}
-
-  next(state: number, event: string): number {
-    if (!this.transitions[state]) return state;
-    return this.transitions[state][event] ?? state;
-  }
-}
